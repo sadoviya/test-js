49c6d9016910ee160fb4f90c0028100ef002ba56
Andrii Sadovyi <sadoviya@gmail.com>
2023-04-14 14:44:21 +0300

    128

--- script.js
+++ script.js
@@ -88,21 +88,21 @@
 // for (const character of string) {
 //   console.log(character);
 // }
-const clients = ["Mango", "Poly", "Ajax"];
-const clientNameToFind = "Poly";
-let message;
+// const clients = ["Mango", "Poly", "Ajax"];
+// const clientNameToFind = "Poly";
+// let message;
 
-for (const client of clients) {
-  // На кожній ітерації будемо перевіряти чи збігається елемент масиву з
-  // іменем клієнта. Якщо збігається - записуємо в message повідомлення
-  // про успіх і робимо break, щоб далі не шукати
-  if (client === clientNameToFind) {
-    message = "Клієнт з таким ім'ям є в базі даних!";
-    break;
-  }
+// for (const client of clients) {
+//   // На кожній ітерації будемо перевіряти чи збігається елемент масиву з
+//   // іменем клієнта. Якщо збігається - записуємо в message повідомлення
+//   // про успіх і робимо break, щоб далі не шукати
+//   if (client === clientNameToFind) {
+//     message = "Клієнт з таким ім'ям є в базі даних!";
+//     break;
+//   }
 
-  // Якщо вони не збігаються - записуємо в message повідомлення про відсутність імені
-  message = "Клієнт з таким ім'ям відсутній в базі даних!";
-}
+//   // Якщо вони не збігаються - записуємо в message повідомлення про відсутність імені
+//   message = "Клієнт з таким ім'ям відсутній в базі даних!";
+// }
 
-console.log(message); // "Клієнт з таким ім'ям є в базі даних!"
+// console.log(message); // "Клієнт з таким ім'ям є в базі даних!"
