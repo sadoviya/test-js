8ac38c014501bb42448a1670b2285aec2706bfdd
Andrii Sadovyi <sadoviya@gmail.com>
2023-04-24 11:02:46 +0300

    135

--- script.js
+++ script.js
@@ -456,17 +456,17 @@
 // console.log(calculateTotalPrice("Blaster"));
 // console.log(calculateTotalPrice("Radar"));
 
-const highTemperatures = {
-  yesterday: 28,
-  today: 26,
-  tomorrow: 33,
-};
+// const highTemperatures = {
+//   yesterday: 28,
+//   today: 26,
+//   tomorrow: 33,
+// };
 // Change code below this line
-const { yesterday, today, tomorrow } = highTemperatures;
+// const { yesterday, today, tomorrow } = highTemperatures;
 // const yesterday = highTemperatures.yesterday;
 // const today = highTemperatures.today;
 // const tomorrow = highTemperatures.tomorrow;
 
 // Change code above this line
-const meanTemperature = (yesterday + today + tomorrow) / 3;
-console.log(meanTemperature);
+// const meanTemperature = (yesterday + today + tomorrow) / 3;
+// console.log(meanTemperature);
