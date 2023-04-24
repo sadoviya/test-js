39e1f4383c97091649dd1f2e64f94e6fada44236
Andrii Sadovyi <sadoviya@gmail.com>
2023-04-24 11:02:02 +0300

    134

--- script.js
+++ script.js
@@ -400,3 +400,73 @@
 //   rgbColors.push(color.rgb);
 // }
 
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// function getProductPrice(productName) {
+// for (const product of products) {
+//   if (product.name === productName) {
+//     return product.price;
+//   }
+//   }
+//   return null;
+// }
+// console.log(getProductPrice("Radar"));
+
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// function getAllPropValues(propName) {
+//   const values = [];
+//   for (const product of products) {
+//     if (product[propName]) {
+//       values.push(product[propName]);
+//     }
+//   }
+//   return values;
+// }
+// console.log(getAllPropValues("name"));
+
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// function calculateTotalPrice(productName) {
+//   // Change code below this line
+//   let totalPrice = 0;
+//   for (let product of products) {
+//     if (product.name === productName) {
+//       totalPrice = product.price * product.quantity;
+//     }
+//   }
+//   return totalPrice;
+//   // Change code above this line
+// }
+// console.log(calculateTotalPrice("Blaster"));
+// console.log(calculateTotalPrice("Radar"));
+
+const highTemperatures = {
+  yesterday: 28,
+  today: 26,
+  tomorrow: 33,
+};
+// Change code below this line
+const { yesterday, today, tomorrow } = highTemperatures;
+// const yesterday = highTemperatures.yesterday;
+// const today = highTemperatures.today;
+// const tomorrow = highTemperatures.tomorrow;
+
+// Change code above this line
+const meanTemperature = (yesterday + today + tomorrow) / 3;
+console.log(meanTemperature);
