0d58bd5714a936be554b7c07e1a29ccca6b8c2bd
Andrii Sadovyi <sadoviya@gmail.com>
2023-05-08 18:21:54 +0300

    141

--- script.js
+++ script.js
@@ -936,3 +936,27 @@ console.table(getUserNames);
 // // Change code above this line
 // console.log(getUserWithEmail(users, "shereeanthony@kog.com"));
 
+// class Storage {
+//   constructor(items) {
+//     this.items = items;
+//   }
+//   getItems() {
+//     return this.items;
+//   }
+//   addItem(newItem) {
+//     this.items.push(newItem);
+//   }
+//   removeItem(itemToRemove) {
+//     this.items = this.items.filter(item => item !== itemToRemove);
+    
+//   }
+// }
+
+
+// // Change code above this line
+// const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+// storage.addItem("Droid");
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+// storage.removeItem("Prolonger");
+// console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
